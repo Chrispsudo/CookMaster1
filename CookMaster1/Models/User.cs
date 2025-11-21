bc355d55ae87e828e693fe6398671d5fef3e977a
Christer <Christer.Fagerstrand@edu.newton.se>
2025-11-21 13:15:15 +0100

    Add project files.

---
+++ CookMaster1/Models/User.cs
@@ -0,0 +1,27 @@
+﻿namespace CookMaster1.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string Country { get; set; }
+
+
+        public bool ValidateLogin(string pw) => Password == pw;
+
+        public void ChangePassword(string newPw)
+        {
+            Password = newPw;
+        }
+
+        public void UpdateDetails(string username, string country)
+        {
+            Username = username;
+
+            Country = country;
+        }
+
+    }
+}
