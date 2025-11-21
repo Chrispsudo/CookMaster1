691d110b306a4f91cafd679e9ca5dd962f16badd
Christer <Christer.Fagerstrand@edu.newton.se>
2025-11-21 13:23:13 +0100

    Startat projektet och satt upp User classen med kommentarer.

--- CookMaster1/Models/User.cs
+++ CookMaster1/Models/User.cs
@@ -2,20 +2,25 @@
 {
     public class User
     {
+        // Users chosen username 
         public string Username { get; set; }
 
+        // Simple password without hashing for demonstration purposes
         public string Password { get; set; }
 
+        // User's country
         public string Country { get; set; }
 
-
+        // Checking if password matches
         public bool ValidateLogin(string pw) => Password == pw;
 
+        // Changing user's password
         public void ChangePassword(string newPw)
         {
             Password = newPw;
         }
 
+        // Updating user's information
         public void UpdateDetails(string username, string country)
         {
             Username = username;
