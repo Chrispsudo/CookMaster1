9e4fae80d078892ce34938b65cec7368739573a4
Christer <Christer.Fagerstrand@edu.newton.se>
2025-11-21 13:54:32 +0100

    Created RecipeManager

---
+++ CookMaster1/Services/RecipeManager.cs
@@ -0,0 +1,50 @@
+﻿using CookMaster1.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CookMaster1.Services
+{
+    public class RecipeManager
+    {
+        // List of all recipes
+
+        public List<Recipe> Recipes { get; set; } = new List<Recipe>();
+
+        // Add new recipe to the list
+
+        public void AddRecipe(Recipe recipe)
+        {
+            Recipes.Add(recipe);
+        }
+
+        // Remove recipe from the list
+        public void RemoveRecipe(Recipe recipe)
+        {
+            Recipes.Remove(recipe);
+        }
+
+        // Get all recipes
+        public List<Recipe> GetAllRecipes()
+        {
+            return Recipes;
+        }
+
+        // Get all recipes by a specific user
+        public List<Recipe> GetRecipesByUser(User user)
+        {
+            return Recipes.Where(r => r.CreatedBy == user).ToList();
+        }
+
+        // Filter recipes based on text
+
+        public List<Recipe> Filter(string Criteria)
+        {
+            return Recipes.Where(r => r.Title.Contains(Criteria) ||
+                                      r.Ingredients.Contains(Criteria) ||
+                                      r.Instructions.Contains(Criteria) ||
+                                      r.Category.Contains(Criteria)).ToList();
+        }
+
+
+    }
+}
