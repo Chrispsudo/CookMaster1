38c5799ff4f5892c8f84f824b9a8c16873aeec06
Christer <Christer.Fagerstrand@edu.newton.se>
2025-11-21 13:30:41 +0100

    AdminUser klass skapad och förbered.

---
+++ CookMaster1/Models/AdminUser.cs
@@ -0,0 +1,21 @@
+﻿namespace CookMaster1.Models
+{
+    // AdminUser class inherits from User class
+    public class AdminUser : User
+    {
+        // Admin specific function to remove any recipe
+
+        public void RemoveAnyRecipe()
+        {
+            // Implementation for removing any recipe
+        }
+
+        // Admin function to show all recipes
+
+        public void ViewAllRecipes()
+        {
+            // Implementation for viewing all recipes
+        }
+
+    }
+}
